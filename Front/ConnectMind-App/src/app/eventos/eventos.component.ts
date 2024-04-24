0f8c996ff8eaf8fb5d673cb8bfc1be0ae4913670
Andre G. S. Paschoa <108034536+andregspaschoa@users.noreply.github.com>
2024-04-24 17:22:01 -0300

    Front-end: Estilizando a Tela Eventos e adicionando as interpolações, diretivas e bindings.

--- Front/ConnectMind-App/src/app/eventos/eventos.component.ts
+++ Front/ConnectMind-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  exibirImagem: boolean = false;
+  private _filterList: string = '';
+
+  public get filterList() {
+    return this._filterList
+  }
+
+  public set filterList(value) {
+    this._filterList = value
+    this.eventosFiltrados = this.filterList ? this.filterEventos(this.filterList) : this.eventos;
+  }
+
+  filterEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: {tema: string; local: string; dataEvento: string; }) =>
+        evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
+        evento.dataEvento.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,10 +40,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  showOrHideImg() {
+    this.exibirImagem = !this.exibirImagem;
+  }
+
   public getEventos(): void {
 
     this.http.get('https://localhost:5001/api/eventos').subscribe(
-      response => this.eventos = response,
+      response => {this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error => console.log(error),
     );
   }
