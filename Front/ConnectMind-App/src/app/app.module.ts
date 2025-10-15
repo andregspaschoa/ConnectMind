205115bd5097f73007bae640e664456201f06572
andregspaschoa <paschoa.andre@outlook.com>
2025-10-15 16:22:23 -0300

    feat: Adiciona ngx-toastr e ngx-spinner, tooltip, e dropdown menu, refatora eventos e implementa modal de confirmação para exclusão

--- Front/ConnectMind-App/src/app/app.module.ts
+++ Front/ConnectMind-App/src/app/app.module.ts
@@ -1,17 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
+import { ToastrModule } from 'ngx-toastr';
+
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
-import { HttpClientModule } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-import { FormsModule } from '@angular/forms';
 import { EventoService } from './services/evento.service';
+
 import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
 
 @NgModule({
@@ -28,9 +37,20 @@ import { DateTimeFormatPipe } from './helpers/DateTimeFormat.pipe';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    CollapseModule.forRoot()
-  ],
+    ToastrModule.forRoot({
+      timeOut: 5000,
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true,
+      progressBar: true,
+    }),
+    NgxSpinnerModule,
+    CollapseModule.forRoot(),
+    TooltipModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot()
+  ], // Erro não está afetando a compilação do codigo.
   providers: [EventoService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class AppModule { }
--- Front/ConnectMind-App/src/app/eventos/eventos.component.ts
+++ Front/ConnectMind-App/src/app/eventos/eventos.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { EventoService } from '../services/evento.service';
 import { Evento } from '../models/Evento';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-eventos',
@@ -8,6 +11,9 @@ import { Evento } from '../models/Evento';
   styleUrls: ['./eventos.component.scss']
 })
 export class EventosComponent implements OnInit {
+
+  modalRef!: BsModalRef;
+
 
   public eventos: Evento[] = [];
   public eventosFiltrados: Evento[] = [];
@@ -33,9 +39,16 @@ export class EventosComponent implements OnInit {
     )
   }
 
-  constructor(private eventoService: EventoService) { }
+  constructor(
+    private eventoService: EventoService, 
+    private modalService: BsModalService,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService
+
+  ) { }
 
   ngOnInit() {
+    this.spinner.show();
     this.getEventos();
   }
 
@@ -44,14 +57,30 @@ export class EventosComponent implements OnInit {
   }
 
   public getEventos(): void {
-
-    this.eventoService.getEventos().subscribe(
-      (_eventos: Evento[]) => {
-        this.eventos = _eventos;
+    this.eventoService.getEventos().subscribe({
+      next: (eventos: Evento[]) => {
+        this.eventos = eventos;
         this.eventosFiltrados = this.eventos;
       },
-      error => console.log(error),
-    );
+      error: (error: any) => {
+        this.spinner.hide();
+        this.toastr.error('Erro ao carregar os eventos', 'Erro!');
+      },
+      complete: () => this.spinner.hide()
+    },);
+  }
+
+  openModal(template: TemplateRef<any>):void {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.modalRef.hide();
+    this.toastr.success('O evento foi deletado com sucesso!', 'Deletado!');
+  }
+
+  decline(): void {
+    this.modalRef.hide();
   }
 
 }
